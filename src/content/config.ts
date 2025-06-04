46d22350347e550ccc1f4f550bc8b60144a8d8a5
MacDaniel Sullivan <sullivan.macdaniel@jamersan.com>
2025-06-03 20:47:47 -0500

    pushy push

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const pages = defineCollection({
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
+    body: z.string().optional(), // optional if you're not using this field
   }),
 });
 
